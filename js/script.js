6ba2dcc4d076d6f86010dc3c56af3c5421a4e9ee
trish <trish.codes@gmail.com>
2016-02-18 15:06:05 -0600

    JQ added

---
+++ js/script.js
@@ -0,0 +1,27 @@
+// script.js
+
+$(function() {
+
+    //Click the go button
+    $('#go').click(function (){
+        
+        //get with of the cars
+        var carWidth = $('#car1').width();
+        
+        //get with of the cars
+        var raceTrackWidth= $(window).width() - carWidth;
+        
+        //generate a random # between 1 and 5000
+        
+        var raceTime1 = Math.floor( (Math.random() * 5000) + 1);
+        var raceTime2 = Math.floor( (Math.random() * 5000) + 1);
+        
+        //set a flag variable to False by default
+        var isComplete = false;
+        //set a flag bariabl to First by default
+        var place = 'first';
+        
+    });
+    
+
+});
